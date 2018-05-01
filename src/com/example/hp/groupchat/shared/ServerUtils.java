b64839e32382836d5dbc0e5c9cdb624341560b37
Alejandro Castellanos <alex810_castellanos@hotmail.com>
2018-04-30 19:21:04 -0500

    SimpleDate

--- src/com/example/hp/groupchat/shared/ServerUtils.java
+++ src/com/example/hp/groupchat/shared/ServerUtils.java
@@ -7,6 +7,7 @@ package com.example.hp.groupchat.shared;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+
 /**
  *
  * @author hp
@@ -14,7 +15,11 @@ import java.util.Calendar;
 public class ServerUtils {
 
     public static String dateLog() {
-               return new SimpleDateFormat("yyyy/MM/dd_HH:mm:ss").format(Calendar.getInstance().getTime());
+        return new SimpleDateFormat("yyyy/MM/dd_HH:mm:ss").format(Calendar.getInstance().getTime());
 
+    }
+
+    public static String simpleDate() {
+        return new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
     }
 }
