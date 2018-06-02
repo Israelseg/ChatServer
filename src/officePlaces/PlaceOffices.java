6698eff2f1bdc4378d0ace5d4ad3647f0cf672e9
Andres <andresfet-12@hotmail.com>
2018-06-02 16:17:13 -0500

    Paquete oficinas

---
+++ src/officePlaces/PlaceOffices.java
@@ -0,0 +1,39 @@
+package officePlaces;
+
+import chatserver.ChatServerThread;
+import com.example.hp.groupchat.shared.KeyWordSystem;
+import com.example.hp.groupchat.shared.Message;
+import entertainmentPlaces.PlaceEntertainment;
+import entertainmentPlaces.PlaceService;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONArray;
+
+public class PlaceOffices extends Thread{
+    
+    private final String order;
+    private final ChatServerThread user;
+
+    public PlaceOffices(String order, ChatServerThread userThread) {
+        this.order = order;
+        this.user = userThread;
+    }
+    
+    @Override
+    public void run() {
+        double lat = 22.25476, lng = -97.8492;
+        JSONArray centrocomputo = new JSONArray();
+        centrocomputo.put(0, order);
+        centrocomputo.put(1, lat);
+        centrocomputo.put(2, lng);
+        Message response = new Message(KeyWordSystem.BOT_NAME, KeyWordSystem.TYPE_MAP, centrocomputo.toString());
+        System.out.println(response.toString());
+        response.setContent(PlaceService.staticMap(lat, lng, 14));
+        try {
+            user.send(response);
+        } catch (Throwable ex) {
+            Logger.getLogger(PlaceEntertainment.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
